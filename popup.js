4009bb386d9be7be86fed9c693ca54e577c94745
shelestovichp <sagitt>
2017-07-29 16:37:17 +1200

    data changed to local

--- popup.js
+++ popup.js
@@ -3,8 +3,18 @@ function renderStatus(statusText) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    $.get("https://home-space.s3.amazonaws.com/data.json", function (data) {
-        var properties = JSON.parse(data);
-        console.log('Data: ' + properties);
-    });
+    // $.get("https://home-space.s3.amazonaws.com/data.json", function (data) {
+    //     var properties = JSON.parse(data);
+    //     console.log('Data: ' + properties);
+    // });
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            var data = JSON.parse(xhr.responseText);
+            console.log('Data: ' + data);
+        }
+    };
+    xhr.open("GET", chrome.extension.getURL('data.json'), true);
+    xhr.send();
 });
