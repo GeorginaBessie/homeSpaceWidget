c47c259fd1c0f15180aa3de819cd59ac0ff5e14c
Georgina Durant <gbdurant@hotmail.com>
2017-07-29 15:24:58 +1200

    added widget

---
+++ popup.js
@@ -0,0 +1,12 @@
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	/* 
+	$.get( "ajax/test.html", function( data ) {
+  $( ".result" ).html( data );
+  alert( "Load was performed." );
+}); */
+
+  });
