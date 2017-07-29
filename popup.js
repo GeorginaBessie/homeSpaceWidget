1d5be46396003ca3fdeb1ce39006fa1b7e0361ea
shelestovichp <sagitt>
2017-07-29 16:26:19 +1200

    Data is loaded

--- popup.js
+++ popup.js
@@ -1,12 +1,10 @@
 function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
+    document.getElementById('status').textContent = statusText;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	/* 
-	$.get( "ajax/test.html", function( data ) {
-  $( ".result" ).html( data );
-  alert( "Load was performed." );
-}); */
-
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    $.get("https://home-space.s3.amazonaws.com/data.json", function (data) {
+        var properties = JSON.parse(data);
+        console.log('Data: ' + properties);
+    });
+});
