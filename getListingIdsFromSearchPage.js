8db1f831d4fce2c43c3d11889996bdc83168fc0c
Dominic <dataylorx@gmail.com>
2017-07-30 13:49:44 +1200

    Add getting auction ids from search page

--- getListingIdsFromSearchPage.js
+++ getListingIdsFromSearchPage.js
@@ -1,19 +1,18 @@
-//getListingId for search page
-console.log('getlistings gfrom seachpage')
+//getListingIds from search page
 chrome.runtime.onMessage.addListener(messageReceivedFromPopup);
+console.log('getlistings from seachpage')
 
 function messageReceivedFromPopup(message, sender, sendResponseBackToPopup) {
     if (message === 'GET_LISTING_ID') {
         chrome.runtime.onMessage.removeListener(messageReceivedFromPopup);
 
-        var listingNumber;
-        // var listingRef = $('#ListingTitle_noStatusListingNumberContainer');
-        var listingRef = document.getElementById('ListingTitle_noStatusListingNumberContainer');
-
-
-        if (listingRef.length) {
-            listingId = listingRef.text().split(' ').pop();
-            listingNumber = parseFloat(listingId)
+        var listingLinks = document.getElementsByClassName('dotted')
+        console.log(parseFloat(listingLinks[0].pathname.split('auction-')[1].split('.')[0]))
+        listingIds = [];
+        if (listingLinks.length) {
+            for (var i = 0; i < listingLinks.length; i++) {
+              listingIds.push(parseFloat(listingLinks[i].pathname.split('auction-')[1].split('.')[0]))  ;
+            }
         }
 
         sendResponseBackToPopup(listingNumber)
